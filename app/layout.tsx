f620aaaaef844999331654db43784631b4d845ea
kkhj218 <kkhj218@gmail.comgit config --global user.name>
2026-07-30 13:41:58 +0900

    feat: add metadata base

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,9 +14,12 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://hohaeng.vercel.app'),
+
   title: '호행처럼 - 스마트한 금융 & 라이프 가이드',
+
   description: '돈과 시간을 아껴주는 스마트한 수치 가이드',
-  // 🔑 구글 서치 콘솔 소유권 확인 태그
+
   verification: {
     google: 'nXO5xT2zh_3lpjUhYw61VwYIhl35XB92KZW0QVJHEvg',
   },
@@ -32,10 +35,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-slate-950 text-slate-100 flex flex-col`}
       >
-        {/* 상단 네비게이션 헤더 */}
         <Header />
 
-        {/* 본문 페이지 영역 */}
         <div className="flex-1">{children}</div>
       </body>
     </html>
